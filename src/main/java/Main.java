34da6f53bc37f86621f046b24c9644fb5d09921e
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-06-07 17:12:34 +0200

    did some test on fizzbuzz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,27 @@ public class Main {
     static public int sum(int a, int b){
         return a + b;
     }
+
+    static String fizzBuzz(int n){
+        return String.valueOf(n);
+    }
+
+    static String fizzBuzzDiv(int n){
+        String result = "";
+        if(n%3 == 0){
+            result = "fizz";
+        }else if(n%5 == 0){
+            result = String.valueOf(n);
+        }
+        if(n % 5 == 0 && n % 3==0) result = "fizzbuzz";
+
+        return result;
+    }
     public static void main(String[] args) {
         System.out.println(sum(10, 10));
         System.out.println("Hello worlddd");
+        System.out.println(fizzBuzz(10));
+        System.out.println(fizzBuzzDiv(3));
+        System.out.println(fizzBuzzDiv(15));
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,4 +8,32 @@ public class MainTest {
         int expected = 4;
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void yields_text_of_input(){
+        String actual = Main.fizzBuzz(10);
+        String expected = "10";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void yields_a_String_fizz(){
+        String actual = Main.fizzBuzzDiv(3);
+        String expected= "fizz";
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    void is_dividable_by_5(){
+        String actual = Main.fizzBuzzDiv(5);
+        String expected = "5";
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    void is_dividable_by_5_and_3(){
+        String actual = Main.fizzBuzzDiv(15);
+        String expected = "fizzbuzz";
+        Assertions.assertEquals(expected, actual);
+    }
 }
